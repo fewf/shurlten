105777d8901a9b05cf721185abcc9dc26132dc44
greggreggreg7 <g.s.altman@gmail.com>
2014-01-16 18:13:25 -0500

    starting work on url shortener

--- web.js
+++ web.js
@@ -1,12 +1,38 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var fs = require("fs")
 var app = express();
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
+app.get('/a', function(req, res) {
   res.send('Hello World!');
+});
+
+app.get(/^\/([A-Za-Z0-9]+)$/, function(req, res) {
+	fs.open('shorten_dict.js', 'r', function(err, fd) {
+		fs.fstat(fd, function(err, stat) {
+			var bufferSize = stats.size;
+			var chunkSize = 512;
+			var buffer = new Buffer(bufferSize);
+			var bytesRead = 0;
+
+			while (bytesRead < bufferSize) {
+				if ((bytesRead + chunkSize) > bufferSize) {
+					chunkSize = (bufferSize - bytesRead);
+				}
+				fs.read(fd, buffer, bytesRead, chunkSize, bytesRead);
+				bytesRead += chunkSize;
+			}
+			var url_dict = JSON.parse(buffer.toString('utf8', 0, bufferSize));
+			if (url_dict[req.param[0]]) {
+				res.redirect(url_dict[req.param[0]]);
+			} else {
+				res.send('Sorry not found.');
+			}
+		});
+	})
 });
 
 var port = process.env.PORT || 5000;
