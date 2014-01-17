05fab4d3a6a46faf546af3ec64a8f9c42f410145
greggreggreg7 <g.s.altman@gmail.com>
2014-01-17 05:07:42 -0500

    testing

--- web.js
+++ web.js
@@ -3,7 +3,6 @@ var express = require("express");
 var logfmt = require("logfmt");
 var mongo = require('mongodb');
 
-
 var app = express();
 
 var mongoUri = process.env.MONGOLAB_URI ||
@@ -11,7 +10,7 @@ var mongoUri = process.env.MONGOLAB_URI ||
   'mongodb://localhost/mydb';
 
 app.use(logfmt.requestLogger());
-
+app.use(express.bodyParser());
 app.get('/', function(req, res) {
   res.send('<input type="text" />');
 });
@@ -29,6 +28,14 @@ app.get(/^\/([\d\w]+)$/, function(req, res) {
 		});
 	});
 });
+
+app.post(/^\/addurl\/(.+)/, function(req, res) {
+	mongo.Db.connect(mongoUri, function (err, db) {
+		db.collection('urls', function(er, collection) {
+			collection.insert({"short": "g5", "url": req.params[0]});
+		});
+	});
+})
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
