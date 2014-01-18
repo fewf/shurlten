5151c0f349e89f13fb55351a180174cc561edf6d
greggreggreg7 <g.s.altman@gmail.com>
2014-01-17 19:05:38 -0500

    testing

--- web.js
+++ web.js
@@ -31,9 +31,9 @@ app.get(/^\/([\d\w]+)$/, function(req, res) {
 });
 
 app.get(/^\/addurl/, function(req, res) {
-	console.log('req before connect callback: ' + req.params[0]);
+	console.log('req before connect callback: ' + JSON.stringify(req));
 	mongo.Db.connect(mongoUri, function (err, db) {
-		console.log('req in connect callback: ' + req.params[0]);
+		console.log('req in connect callback: ' + JSON.stringify(req));
 		db.collection('urls', function(er, collection) {
 			collection.insert({"short": "g5", "url": req.params[0]});
 		});
