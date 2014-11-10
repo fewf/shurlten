9572187b6d6c6117b5f7bfe6d5a4004ba824be69
greggreggreg7 <g.s.altman@gmail.com>
2014-11-09 19:27:42 -0500

    cleaned up comments and debugging console writes

--- web.js
+++ web.js
@@ -13,10 +13,6 @@ app.use(logfmt.requestLogger());
 app.use(express.urlencoded());
 app.use(express.static(__dirname + '/public'));
 
-// Optional since express defaults to CWD/views
-
-// app.set('views', '');
-
 // Set our default template engine to "jade"
 // which prevents the need for extensions
 // (although you can still mix and match)
@@ -25,7 +21,6 @@ app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
     res.render('main');
-  // res.sendfile('./main.html');
 });
 
 if (!global.db) {
@@ -44,9 +39,7 @@ app.get(/^\/([\d\w]+)$/, function(req, res) {
             if (docs.length) {
                 res.redirect(docs[0].url);
             } else {
-                
                 res.render('sub', { site: false });
-                // res.send('Sorry not found.');
             }
         });
     });
@@ -56,12 +49,9 @@ app.get('/addurl/', function(req, res) {
 
     getSeqColl(getAndIncSeq);
 
-
-
     function getSeqColl(callback) {    
         global.db.collection('ref_seq', function(err, collection) {
             if (!err) {
-                console.log("didn't error in getSeqColl")
                 // getAndIncSeq
                 callback(collection, getURLsColl);
             } else {
@@ -71,22 +61,20 @@ app.get('/addurl/', function(req, res) {
     }
 
     function getAndIncSeq(collection, callback) {
-            collection.findAndModify({ _id: "seq" }, {}, { $inc: { seq: 1 }},
-                                 {}, function(err, object) {
-                if (!err) {
-                    console.log("didn't error in getanincseq")
-                    console.log(JSON.stringify(object));
-                    // getURLsColl
-                    callback(object, addNewShort);
-                } else {
-                    throw new Error(err);
-                }
+        collection.findAndModify({ _id: "seq" }, {}, { $inc: { seq: 1 }},
+                             {}, function(err, object) {
+            if (!err) {
+                // getURLsColl
+                callback(object, addNewShort);
+            } else {
+                throw new Error(err);
+            }
         });
 	}
 
     function getURLsColl(object, callback) {
         global.db.collection('urls', function(err, collection) {
-            console.log("appears to have gotten urls collection ok")
+            // addNewShort
             callback(req, res, collection, genID(object.seq), sendToShortened);
         });
     }
@@ -94,13 +82,13 @@ app.get('/addurl/', function(req, res) {
 	function addNewShort(req, res, collection, seq, callback) {
 	        var url = req.query.url;
             collection.insert({"short": seq, "url": url}, function() {
+                // sendToShortened
                 callback(seq);
             });
 	}
 
 	function sendToShortened(link) {
         res.render('sub', { site: 'http://shurlten.herokuapp.com/' + link });
-	    // res.send(genResponse(link));
 	}
 
 });
@@ -110,13 +98,6 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });    
-
-// function genResponse(link) {   
-//     return 'here\'s your link: <a href="http://shurlten.herokuapp' +
-//             '.com/'  + link + '" target="_blank">shurlten' +
-//             '.herokuapp.com/'  + link + '<br />' +
-//             '<a href="https://github.com/fewf/url_shortener" target="_blank">github</a>';
-// }
 
 function genID(decimal, symbols) {
     console.log("start genID: " + decimal);
@@ -144,7 +125,6 @@ function genID(decimal, symbols) {
         i++;
 
     }
-    console.log("got out of first while")
     while ( i >= 0 ) {
 
         var leftMost = symbols[Math.floor(decimal / pow(i))];
@@ -152,6 +132,6 @@ function genID(decimal, symbols) {
         decimal = decimal - pow(i) * Math.floor(decimal / pow(i));
         i--;
     }
-    console.log("end genid");
+
     return numstr;
 }
